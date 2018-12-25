cba142dc44418f066b7de63073fabb0f5d193012
Vin <chanvs-wa16@student.tarc.edu.my>
2018-12-25 16:20:16 +0800

    update

---
+++ app/src/main/java/com/example/vin/myapplication/Login.java
@@ -0,0 +1,34 @@
+package com.example.vin.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    private Button buttonReigster;
+    private EditText txtEmail;
+    private EditText txtPassword;
+    private TextView txtRegister;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtRegister = (TextView)findViewById(R.id.txtRegister);
+
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
+
+    }
+}
