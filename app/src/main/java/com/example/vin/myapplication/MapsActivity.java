7c51856e9443cf20fd73258ec3e7bcbbd1460319
Vin <chanvs-wa16@student.tarc.edu.my>
2018-12-29 14:29:57 +0800

    LOGIN DONE LOGOUT DONE REGISTER DONE (FIX) LOCATION (NOT DONE)
    NAVIGATION DONE (FIX)
    ADD NAME AND EMAIL TO NAVIGATOR
    
    ADD REAL-TIME DATABASE

--- app/src/main/java/com/example/vin/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/vin/myapplication/MapsActivity.java
@@ -10,6 +10,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -38,14 +40,17 @@ public class MapsActivity extends FragmentActivity implements
     private LocationRequest locationRequest;
     private Location lastLocation;
     private Marker currentUserLocationMarker;
-    private static  final int Request_User_Location_Code =  99;
+    private static final int Request_User_Location_Code = 99;
+    private double latitude, longitude;
+    private int proximityRadius = 10000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkUserLocationPermission();
         }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -59,7 +64,7 @@ public class MapsActivity extends FragmentActivity implements
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
 
             buildGoogleApiClient();
@@ -69,18 +74,17 @@ public class MapsActivity extends FragmentActivity implements
         }
     }
 
-    public boolean checkUserLocationPermission(){
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
-                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},Request_User_Location_Code);
-            }else{
-                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},Request_User_Location_Code);
+    public boolean checkUserLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Request_User_Location_Code);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Request_User_Location_Code);
 
             }
             return false;
 
-        }
-        else{
+        } else {
             return true;
         }
     }
@@ -88,24 +92,24 @@ public class MapsActivity extends FragmentActivity implements
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-       switch(requestCode){
+        switch (requestCode) {
 
-           case Request_User_Location_Code:
-               if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                   if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED){
-                       if(googleApiClient == null){
-                           buildGoogleApiClient();
-                       }
-                       mMap.setMyLocationEnabled(true);
-                   }
-                   else{
-                       Toast.makeText(this,"Permission Denied ..." , Toast.LENGTH_SHORT).show();
-                   }
-               }
-       }
+            case Request_User_Location_Code:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        if (googleApiClient == null) {
+                            buildGoogleApiClient();
+                        }
+                        mMap.setMyLocationEnabled(true);
+                    } else {
+                        Toast.makeText(this, "Permission Denied ...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        }
     }
 
-    protected synchronized void buildGoogleApiClient(){
+
+    protected synchronized void buildGoogleApiClient() {
         googleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -116,12 +120,16 @@ public class MapsActivity extends FragmentActivity implements
 
     @Override
     public void onLocationChanged(Location location) {
+
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+
         lastLocation = location;
-        if(currentUserLocationMarker != null){
+        if (currentUserLocationMarker != null) {
             currentUserLocationMarker.remove();
         }
 
-        LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
 
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
@@ -133,8 +141,8 @@ public class MapsActivity extends FragmentActivity implements
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.animateCamera(CameraUpdateFactory.zoomBy(14));
 
-        if(googleApiClient != null){
-            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient,this);
+        if (googleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
 
         }
     }
@@ -147,8 +155,8 @@ public class MapsActivity extends FragmentActivity implements
         locationRequest.setFastestInterval(1100);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,locationRequest,this);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
 
         }
 
@@ -162,5 +170,72 @@ public class MapsActivity extends FragmentActivity implements
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    public void onClick(View view) {
+
+        String hospital = "hospital", bank = "bank", restaurant = "cafe|restaurant|restaurants|food", gasStation = "gas_station";
+        Object transferData[] = new Object[2];
+        GetNearbyPlaces getNearbyPlaces = new GetNearbyPlaces();
+
+
+        switch (view.getId()) {
+            case R.id.hospitals_nearby:
+                mMap.clear();
+                String url = getUrl(latitude, longitude, hospital);
+                transferData[0] = mMap;
+                transferData[1] = url;
+
+                getNearbyPlaces.execute(transferData);
+                Toast.makeText(this, "Searching for Nearby Hospital", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Showing Nearby Hospital", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.restaurant_nearby:
+                mMap.clear();
+                String url1 = getUrl(latitude, longitude, restaurant);
+                transferData[0] = mMap;
+                transferData[1] = url1;
+
+                getNearbyPlaces.execute(transferData);
+                Toast.makeText(this, "Searching for Nearby Restaurant", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Showing Nearby Restaurant", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.gas_station_nearby:
+                mMap.clear();
+                String url2 = getUrl(latitude,longitude,gasStation);
+                transferData[0] = mMap;
+                transferData[1] = url2;
+
+                getNearbyPlaces.execute(transferData);
+                Toast.makeText(this,"Searching for Nearby Gas Station" ,Toast.LENGTH_SHORT).show();
+                Toast.makeText(this,"Showing Nearby Gas Station" ,Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.bank_nearby:
+                mMap.clear();
+                String url3 = getUrl(latitude,longitude,bank);
+                transferData[0] = mMap;
+                transferData[1] = url3;
+
+                getNearbyPlaces.execute(transferData);
+                Toast.makeText(this,"Searching for Nearby Bank" ,Toast.LENGTH_SHORT).show();
+                Toast.makeText(this,"Showing Nearby Bank" ,Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    private String getUrl(double latitude, double longitude, String nearbyPlace) {
+        StringBuilder googleURL = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googleURL.append("location=" + latitude + "," + longitude);
+        googleURL.append("&radius=" + proximityRadius);
+        googleURL.append("&type=" + nearbyPlace);
+        googleURL.append("&sensor=true");
+        googleURL.append("&key=" + "AIzaSyBc-HrjK2Yl1Ov3bEO0bRlLSJpRBhiqh3I");
+
+        Log.d("GoogleMapsActivity" , "url = " + googleURL.toString());
+
+        return googleURL.toString();
     }
 }
