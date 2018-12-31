7ec9a69da749eb75e19149fe525f715105ddad5f
Vin <chanvs-wa16@student.tarc.edu.my>
2018-12-31 20:46:57 +0800

    asd

--- app/src/main/java/com/example/vin/myapplication/Report/Report.java
+++ app/src/main/java/com/example/vin/myapplication/Report/Report.java
@@ -1,20 +1,31 @@
 package com.example.vin.myapplication.Report;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.PermissionRequest;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,9 +46,21 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class Report extends Fragment implements
@@ -67,6 +90,10 @@ public class Report extends Fragment implements
     EditText txtTitle;
     String username;
     String chkID;
+    private Button btn;
+    private ImageView imageview;
+    private static final String IMAGE_DIRECTORY = "/demonuts";
+    private int GALLERY = 1, CAMERA = 2;
     // for location
     private boolean mTrackingLocation;
     private FusedLocationProviderClient mFusedLocationClient;
@@ -80,7 +107,7 @@ public class Report extends Fragment implements
         txtLocation = view.findViewById(R.id.txtLocation);
         txtDescription = view.findViewById(R.id.txtDescription);
         txtTitle = view.findViewById(R.id.txtReportTitle);
-        mLocationButton =(Button) view.findViewById(R.id.btnLocation);
+        mLocationButton = (Button) view.findViewById(R.id.btnLocation);
         mLocationTextView = (TextView) view.findViewById(R.id.txtLocation);
         //firedata base
         mAuth = FirebaseAuth.getInstance();
@@ -89,13 +116,24 @@ public class Report extends Fragment implements
         ReportsRef = FirebaseDatabase.getInstance().getReference().child("Reports");
         FirebaseUser firebaseUser = mAuth.getCurrentUser();
         userID = firebaseUser.getUid();
+
+        btn = (Button) view.findViewById(R.id.btn);
+        imageview = (ImageView) view.findViewById(R.id.iv);
+        requestMultiplePermissions();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPictureDialog();
+            }
+        });
+
         //Count reports
         ReportsRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
+                if (dataSnapshot.exists()) {
                     countId += (int) dataSnapshot.getChildrenCount() + 1;
-                }else{
+                } else {
                     countId = 1001;
                 }
             }
@@ -110,7 +148,7 @@ public class Report extends Fragment implements
         mDatabaseUser_name.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                username =dataSnapshot.getValue(String.class);
+                username = dataSnapshot.getValue(String.class);
 
             }
 
@@ -153,6 +191,137 @@ public class Report extends Fragment implements
         return view;
     }
 
+    private void showPictureDialog() {
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(getActivity());
+        pictureDialog.setTitle("Select Action");
+        String[] pictureDialogItems = {
+                "Select photo from gallery",
+                "Capture photo from camera"};
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                choosePhotoFromGallary();
+                                break;
+                            case 1:
+                                takePhotoFromCamera();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+    }
+
+    public void choosePhotoFromGallary() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+
+    private void takePhotoFromCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == getActivity().RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == GALLERY) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), contentURI);
+                    String path = saveImage(bitmap);
+                    Toast.makeText(getActivity(), "Image Saved!", Toast.LENGTH_SHORT).show();
+                    imageview.setImageBitmap(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(), "Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        } else if (requestCode == CAMERA) {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            imageview.setImageBitmap(thumbnail);
+            saveImage(thumbnail);
+            Toast.makeText(getActivity(), "Image Saved!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        // have the object build the directory structure, if needed.
+        if (!wallpaperDirectory.exists()) {
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(getActivity(),
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+    }
+
+    private void requestMultiplePermissions() {
+        Dexter.withActivity(getActivity())
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // check if all permissions are granted
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(getActivity(), "All permissions are granted by user!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // check for permanent denial of any permission
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            // show alert dialog navigating to Settings
+                            //openSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<com.karumi.dexter.listener.PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+
+
+                }).
+                withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getActivity(), "Some Error! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
 
 
     private void startTrackingLocation() {
@@ -184,23 +353,23 @@ public class Report extends Fragment implements
             final String title = txtTitle.getText().toString().trim();
             final String description = txtDescription.getText().toString().trim();
             //Check if title and description is empty
-            if(TextUtils.isEmpty(title)){
-                Toast.makeText(getActivity(),"Please enter title",Toast.LENGTH_LONG).show();
+            if (TextUtils.isEmpty(title)) {
+                Toast.makeText(getActivity(), "Please enter title", Toast.LENGTH_LONG).show();
                 return;
             }
-            if(TextUtils.isEmpty(description)){
-                Toast.makeText(getActivity(),"Please enter reports description",Toast.LENGTH_LONG).show();
+            if (TextUtils.isEmpty(description)) {
+                Toast.makeText(getActivity(), "Please enter reports description", Toast.LENGTH_LONG).show();
                 return;
             }
             chkID = String.valueOf(countId);
-            Reports reports = new Reports(username,location,title,description,date,countId);
+            Reports reports = new Reports(username, location, title, description, date, countId);
             FirebaseDatabase.getInstance().getReference("Reports")
                     .child(chkID)
-                    .setValue(reports).addOnCompleteListener(new OnCompleteListener<Void>(){
+                    .setValue(reports).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
-                public void onComplete(@NonNull Task<Void> task){
-                    if(task.isSuccessful()){
-                        Toast.makeText(getActivity(),"Send Successful",Toast.LENGTH_LONG).show();
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getActivity(), "Send Successful", Toast.LENGTH_LONG).show();
                         Intent intent = new Intent(getActivity(), MainMenu.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(intent);
